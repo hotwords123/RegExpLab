77101b4ca0e1aeb7099de7838876796f36451f93
hotwords123 <hotwords123@gmail.com>
2023-05-08 00:02:44 +0800

    lab2(antlr): refine gui tool script

--- antlr/testRig.sh
+++ antlr/testRig.sh
@@ -7,6 +7,9 @@ ANTLR_JAR="antlr-4.12.0-complete.jar"
 CLASS_OUTPUT_PATH="parser-classes"
 
 cd "$(dirname $0)"
-$JAVAC -cp $ANTLR_JAR parser/*.java -d $CLASS_OUTPUT_PATH
-echo "请输入要解析的字符串，输入完后请关闭输入流(Linux\Mac下按2次Ctrl+D，Windows下回车再Ctrl+Z再回车)："
-$JAVA -cp $ANTLR_JAR:$CLASS_OUTPUT_PATH org.antlr.v4.gui.TestRig regex regex -gui -tokens
+if [ ! -d $CLASS_OUTPUT_PATH ]; then
+  $JAVAC -cp $ANTLR_JAR parser/*.java -d $CLASS_OUTPUT_PATH
+fi
+
+read -p "Input regex: " -r input
+printf "%s" $input | $JAVA -cp "$ANTLR_JAR;$CLASS_OUTPUT_PATH" org.antlr.v4.gui.TestRig regex regex -gui -tokens
