039e3f15388dbbd188cdaa03bcc0d4432a2844c8
hotwords123 <hotwords123@gmail.com>
2023-05-09 16:52:16 +0800

    lab2: auto recompile on testRig if grammar has changed

--- antlr/testRig.sh
+++ antlr/testRig.sh
@@ -7,7 +7,16 @@ ANTLR_JAR="antlr-4.12.0-complete.jar"
 CLASS_OUTPUT_PATH="parser-classes"
 
 cd "$(dirname $0)"
+
+if ! diff regex.g4 "$CLASS_OUTPUT_PATH/regex.g4"; then
+  echo "cleaning old parser classes"
+  rm -r $CLASS_OUTPUT_PATH
+fi
+
 if [ ! -d $CLASS_OUTPUT_PATH ]; then
+  echo "compiling parser classes"
+  mkdir $CLASS_OUTPUT_PATH
+  cp regex.g4 "$CLASS_OUTPUT_PATH/"
   $JAVAC -cp $ANTLR_JAR parser/*.java -d $CLASS_OUTPUT_PATH
 fi
 
