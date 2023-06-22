80f2d330414c85e2d34cbffb4ccec6573337882a
hotwords123 <hotwords123@gmail.com>
2023-06-22 09:31:54 +0800

    lab3: add document and pack script

--- pack.sh
+++ pack.sh
@@ -9,7 +9,7 @@ rm $OUTPUT
 mkdir -p $TEMP_DIR/cpp
 cp -r -t $TEMP_DIR/cpp/ *.cpp *.h CMakeLists.txt parser/ lib/
 strip build/regex.exe
-cp -t $TEMP_DIR/ README.txt build/regex.exe antlr/regex.g4
+cp -t $TEMP_DIR/ README.txt build/regex.exe antlr/regex.g4 document.pdf
 7z a $OUTPUT $TEMP_DIR/*
 rm -r $TEMP_DIR
 7z l $OUTPUT
